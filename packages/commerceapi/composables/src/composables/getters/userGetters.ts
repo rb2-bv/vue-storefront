db4d13fae7266ad3ffed5ade3fbd7691b78d0db1
Carlo Kok <carlo@rb2.nl>
2020-05-08 11:14:03 +0200

    Hook up account personal details to existing apis (commerceapi)

--- packages/commerceapi/composables/src/composables/getters/userGetters.ts
+++ packages/commerceapi/composables/src/composables/getters/userGetters.ts
@@ -6,13 +6,15 @@ import { UserInfo } from '../../types';
 export const getUserFirstName = (user: UserInfo): string => user?.firstName ? user.firstName : '';
 
 export const getUserLastName = (user: UserInfo): string => user?.lastName ? user.lastName : '';
+export const getUserEmail = (user: UserInfo): string => user?.email ? user.email : '';
 
 export const getUserFullName = (user: UserInfo): string => user ? `${user.firstName} ${user.lastName}` : '';
 
 const userGetters: UserGetters<UserInfo> = {
   getFirstName: getUserFirstName,
   getLastName: getUserLastName,
-  getFullName: getUserFullName
+  getFullName: getUserFullName,
+  getEmail: getUserEmail,
 };
 
 export default userGetters;
